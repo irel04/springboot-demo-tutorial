fc8dff8cf5406c79dc8d418be9e52a91a80218a7
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-04-15 20:21:24 +0800

    feat: add delete and put controller

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -12,11 +12,15 @@ class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-      log.info("Preloading " + repository.save(new Employee("Frodo",  "Baggins", "thief")));
+      log.info("Preloading " + employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar")));
+      log.info("Preloading " + employeeRepository.save(new Employee("Frodo",  "Baggins", "thief")));
+
+      
+
+      
     };
   }
 }
